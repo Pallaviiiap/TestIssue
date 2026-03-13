0b0c32f52f8671cc78bac6ff5df8b0d09ec5fa42
Pallaviiiap <pallavi001ada@gmail.com>
2026-03-13 19:08:53 +0530

    Issue Detectio

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+users = {
+    "admin": "1234",
+    "user": "abcd"
+}
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # FIX: check if username exists
+    if username in users and users[username] == password:
+        message = "Login Successful"
+    else:
+        message = "Invalid username or password"
+
+    return render_template("result.html", message=message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
